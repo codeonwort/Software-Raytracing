3f05f36114463edc12c1c146bc23114ec6ee291e
codeonwort <codeonwort@gmail.com>
2019-08-14 22:47:54 +0900

    Build FreeImage using msbuild

--- Setup.ps1
+++ Setup.ps1
@@ -1,4 +1,10 @@
 #
+# TODO: Variables that need auto-detect ###
+#
+$msbuild_path = "C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe"
+
+
+#
 # Download third party libraries
 #
 
@@ -20,11 +26,13 @@ if (Test-Path $freeimage_path -PathType Leaf) {
 	Write-Host "Download finished."
 }
 
+
 #
 # Build third party libraries
 #
 
 ### FreeImage
+# 1. Unzip
 if (Test-Path "$external_dir/FreeImage") {
 	Write-Host "FreeImage already unzipped. skip unzip."
 } else {
@@ -32,4 +40,21 @@ if (Test-Path "$external_dir/FreeImage") {
 	Expand-Archive -Path $freeimage_path -DestinationPath $external_dir
 	Write-Host "Unzip done"
 }
-
+# 2. Build
+Write-Host "Build FreeImage (x64 | release)"
+& $msbuild_path ./external/FreeImage/FreeImage.2017.sln -t:FreeImage -p:Configuration=Release -p:Platform=x64
+# 3. Copy
+$freeimage_source_dir = "./thirdparty/FreeImage/source"
+$freeimage_binary_dir = "./thirdparty/FreeImage/binaries"
+Write-Host "Copy source to $freeimage_source_dir"
+Write-Host "Copy binaries to $freeimage_binary_dir"
+if (!(Test-Path $freeimage_source_dir)) {
+	New-Item -ItemType Directory -Force -Path $freeimage_source_dir
+}
+if (!(Test-Path $freeimage_binary_dir)) {
+	New-Item -ItemType Directory -Force -Path $freeimage_binary_dir
+}
+Copy-Item "./external/FreeImage/Source" -Destination $freeimage_source_dir -Recurse
+Copy-Item "./external/FreeImage/x64/Release/FreeImage.dll" -Destination $freeimage_binary_dir
+Copy-Item "./external/FreeImage/x64/Release/FreeImage.pdb" -Destination $freeimage_binary_dir
+Copy-Item "./external/FreeImage/x64/Release/FreeImage.lib" -Destination $freeimage_binary_dir
