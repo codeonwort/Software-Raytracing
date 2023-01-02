a403b4dad6aa2504f4023432a04b40dbd84a145b
codeonwort <codeonwort@gmail.com>
2023-01-02 22:15:54 +0900

    Stub code for Platform Invoke

--- gui-app/gui-app/MainForm.cs
+++ gui-app/gui-app/MainForm.cs
@@ -2,21 +2,38 @@ using System.Reflection;
 
 namespace gui_app
 {
+    // I have to do this for every file? :/
+    using OBJModelHandle = System.UInt64;
+
+    record vec3(float x, float y, float z);
+
+    record OBJSceneDesc(
+        string sceneName,
+        string objPath,
+        vec3 cameraLocation,
+        vec3 cameraLookAt);
+
     public partial class MainForm : Form
     {
+        private static OBJSceneDesc[] sceneDescs = {
+            new(
+                "CornellBox",
+                "content/cornell_box/CornellBox-Mirror.obj",
+                new(0.0f, 1.0f, 4.0f),
+                new(0.0f, 1.0f, -1.0f)
+            )
+        };
+
         public MainForm()
         {
             InitializeComponent();
-
-            //string raylibPath = Path.GetFullPath("./raylib.dll");
-            //raylibDLL = Assembly.LoadFile(raylibPath);
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
             loggerBox.AppendText("= Software Raytracer =" + Environment.NewLine);
-            
-            if (raylibDLL == null)
+
+            if (RaylibWrapper.Raylib_Initialize() == 0)
             {
                 loggerBox.AppendText("Failed to load raylib.dll" + Environment.NewLine);
             }
@@ -25,6 +42,7 @@ namespace gui_app
                 loggerBox.AppendText("Load raylib.dll" + Environment.NewLine);
             }
 
+#if false
             string[] sceneDescs = {
                 "CornellBox",
                 "BreakfastRoom",
@@ -34,9 +52,10 @@ namespace gui_app
                 "SibenikCathedral",
                 "SanMiguel",
             };
-            foreach (string sceneDesc in sceneDescs)
+#endif
+            foreach (OBJSceneDesc sceneDesc in sceneDescs)
             {
-                sceneList.Items.Add(sceneDesc);
+                sceneList.Items.Add(sceneDesc.sceneName);
             }
             sceneList.SelectedIndex = 0;
         }
@@ -44,7 +63,29 @@ namespace gui_app
         private void executeButton_Click(object sender, EventArgs e)
         {
             loggerBox.AppendText(String.Format("viewport: {0}, {1}", viewport.Width, viewport.Height) + Environment.NewLine);
-            
+
+            if (sceneList.SelectedIndex < 0 && sceneList.SelectedIndex >= sceneDescs.Length)
+            {
+                return;
+            }
+
+            OBJSceneDesc sceneDesc = sceneDescs[sceneList.SelectedIndex];
+            string fullpath = Path.GetFullPath("../../../../../" + sceneDesc.objPath);
+
+            OBJModelHandle objHandle = RaylibWrapper.Raylib_LoadOBJModel(fullpath);
+            if (objHandle == 0)
+            {
+                loggerBox.AppendText("Failed to load " + fullpath);
+            }
+            else
+            {
+                loggerBox.AppendText("Load " + fullpath);
+            }
+
+            // TODO: Render the loaded OBJ model
+            // ...
+
+            // Create a bitmap to update the viewport.
             Bitmap renderTarget = new Bitmap(
                 viewport.Width,
                 viewport.Height,
@@ -78,7 +119,5 @@ namespace gui_app
 
             viewport.Image = renderTarget;
         }
-
-        private Assembly raylibDLL;
     }
 }
---
+++ gui-app/gui-app/RaylibWrapper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gui_app
+{
+    using OBJModelHandle = System.UInt64;
+
+    // Wrapper for raylib.dll which is built from my C++ project.
+    internal static class RaylibWrapper
+    {
+        // -----------------------------------------------------------------------
+        // Library initialization & termination
+
+        [DllImport("raylib.dll")]
+        internal static extern int Raylib_Initialize();
+
+        [DllImport("raylib.dll")]
+        internal static extern int Raylib_Terminate();
+
+        // -----------------------------------------------------------------------
+        // Manage media files
+
+        [DllImport("raylib.dll")]
+        internal static extern OBJModelHandle Raylib_LoadOBJModel(String objPath);
+
+        //Raylib_TransformOBJModel
+        //Raylib_FinalizeOBJModel
+        //Raylib_UnloadOBJModel
+        //Raylib_LoadImage
+
+        // -----------------------------------------------------------------------
+        // Scene
+
+        //Raylib_CreateScene
+        //Raylib_AddSceneElement
+        //Raylib_AddOBJModelToScene
+        //Raylib_SetSkyPanorama
+        //Raylib_SetSunIlluminance
+        //Raylib_SetSunDirection
+        //Raylib_FinalizeScene
+        //Raylib_DestroyScene
+        //Raylib_CreateCamera
+        //Raylib_CameraSetPosition
+        //Raylib_CameraSetLookAt
+        //Raylib_CameraSetPerspective
+        //Raylib_CameraSetLens
+        //Raylib_CameraSetMotion
+        //Raylib_CameraCopy
+        //Raylib_DestroyCamera
+        //Raylib_CreateImage
+        //Raylib_DestroyImage
+
+        // -----------------------------------------------------------------------
+        // Rendering
+
+        //Raylib_Render
+        //Raylib_Denoise
+        //Raylib_PostProcess
+        //Raylib_IsDenoiserSupported
+
+        // -----------------------------------------------------------------------
+        // Utils
+
+        //Raylib_GetRenderModeString
+        //Raylib_WriteImageToDisk
+        //Raylib_FlushLogThread
+    }
+}
