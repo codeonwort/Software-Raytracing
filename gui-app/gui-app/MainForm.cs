58cdfb4a842eecf30d09b9ddb113543f5d22b13c
codeonwort <codeonwort@gmail.com>
2023-01-02 23:46:36 +0900

    WIP: Render using raylib

--- gui-app/gui-app/MainForm.cs
+++ gui-app/gui-app/MainForm.cs
@@ -1,9 +1,13 @@
+using System.Diagnostics;
 using System.Reflection;
 
 namespace gui_app
 {
     // I have to do this for every file? :/
     using OBJModelHandle = System.UInt64;
+    using SceneHandle = System.UInt64;
+    using CameraHandle = System.UInt64;
+    using ImageHandle = System.UInt64;
 
     record vec3(float x, float y, float z);
 
@@ -69,21 +73,61 @@ namespace gui_app
                 return;
             }
 
+            uint viewportWidth = (uint)viewport.Width;
+            uint viewportHeight = (uint)viewport.Height;
+
+            SceneHandle sceneHandle = RaylibWrapper.Raylib_CreateScene();
+            CameraHandle cameraHandle = RaylibWrapper.Raylib_CreateCamera();
+            ImageHandle mainImage = RaylibWrapper.Raylib_CreateImage(viewportWidth, viewportHeight);
+
+            // TODO: Hard-coded only for x64 config
             OBJSceneDesc sceneDesc = sceneDescs[sceneList.SelectedIndex];
-            string fullpath = Path.GetFullPath("../../../../../" + sceneDesc.objPath);
+            string fullpath = Path.GetFullPath("../../../../../../" + sceneDesc.objPath);
 
             OBJModelHandle objHandle = RaylibWrapper.Raylib_LoadOBJModel(fullpath);
             if (objHandle == 0)
             {
-                loggerBox.AppendText("Failed to load " + fullpath);
+                loggerBox.AppendText("Failed to load " + fullpath + Environment.NewLine);
             }
             else
             {
-                loggerBox.AppendText("Load " + fullpath);
+                loggerBox.AppendText("Load " + fullpath + Environment.NewLine);
             }
 
-            // TODO: Render the loaded OBJ model
-            // ...
+            RaylibWrapper.Raylib_AddOBJModelToScene(sceneHandle, objHandle);
+
+            RaylibWrapper.Raylib_FinalizeScene(sceneHandle);
+
+            RaylibWrapper.Raylib_CameraSetPosition(cameraHandle, sceneDesc.cameraLocation.x, sceneDesc.cameraLocation.y, sceneDesc.cameraLocation.z);
+            RaylibWrapper.Raylib_CameraSetLookAt(cameraHandle, sceneDesc.cameraLookAt.x, sceneDesc.cameraLookAt.y, sceneDesc.cameraLookAt.z);
+            RaylibWrapper.Raylib_CameraSetPerspective(cameraHandle, 60.0f, (float)viewportWidth / viewportHeight);
+            RaylibWrapper.Raylib_CameraSetLens(cameraHandle, 0.01f, 5.0f);
+            RaylibWrapper.Raylib_CameraSetMotion(cameraHandle, 0.0f, 0.0f);
+
+            RaylibWrapper.RendererSettings settings = new RaylibWrapper.RendererSettings();
+            settings.viewportWidth = viewportWidth;
+            settings.viewportHeight = viewportHeight;
+            settings.samplesPerPixel = 10;
+            settings.maxPathLength = 5;
+            settings.rayTMin = 0.0001f;
+            settings.renderMode = 0;
+
+            loggerBox.AppendText("Render..." + Environment.NewLine);
+            Debug.WriteLine("Render...");
+
+            // TODO: Crashes here (access violation?)
+            RaylibWrapper.Raylib_Render(ref settings, sceneHandle, cameraHandle, mainImage);
+
+            loggerBox.AppendText("PostProcess..." + Environment.NewLine);
+            Debug.WriteLine("PostProcess...");
+
+            RaylibWrapper.Raylib_PostProcess(mainImage);
+
+            loggerBox.AppendText("Display..." + Environment.NewLine);
+            Debug.WriteLine("Display...");
+
+            float[] mainImageData = new float[viewportWidth * viewportHeight * 3];
+            RaylibWrapper.Raylib_DumpImageData(mainImage, mainImageData);
 
             // Create a bitmap to update the viewport.
             Bitmap renderTarget = new Bitmap(
@@ -102,15 +146,20 @@ namespace gui_app
 
             // Fill random data.
             // TODO: Fill ray traced image.
+            int k = 0;
             for (int y = 0; y < renderTarget.Height; ++y)
             {
                 for (int x = 0; x < renderTarget.Width; ++x)
                 {
                     int p = y * rawBuffer.Stride + (3 * x);
-                    byte k = (byte)((x ^ y) & 0xff);
-                    rgbValues[p + 0] = k;
-                    rgbValues[p + 1] = k;
-                    rgbValues[p + 2] = k;
+                    //byte k = (byte)((x ^ y) & 0xff);
+                    //rgbValues[p + 0] = k;
+                    //rgbValues[p + 1] = k;
+                    //rgbValues[p + 2] = k;
+                    rgbValues[p + 0] = (byte)(mainImageData[k + 0] * 255.0f);
+                    rgbValues[p + 1] = (byte)(mainImageData[k + 1] * 255.0f);
+                    rgbValues[p + 2] = (byte)(mainImageData[k + 2] * 255.0f);
+                    k += 3;
                 }
             }
 
@@ -118,6 +167,12 @@ namespace gui_app
             renderTarget.UnlockBits(rawBuffer);
 
             viewport.Image = renderTarget;
+
+            // Cleanup
+            RaylibWrapper.Raylib_UnloadOBJModel(objHandle);
+            RaylibWrapper.Raylib_DestroyScene(sceneHandle);
+            RaylibWrapper.Raylib_DestroyCamera(cameraHandle);
+            RaylibWrapper.Raylib_DestroyImage(mainImage);
         }
     }
 }
--- gui-app/gui-app/RaylibWrapper.cs
+++ gui-app/gui-app/RaylibWrapper.cs
@@ -8,10 +8,26 @@ using System.Threading.Tasks;
 namespace gui_app
 {
     using OBJModelHandle = System.UInt64;
+    using SceneHandle = System.UInt64;
+    using CameraHandle = System.UInt64;
+    using ImageHandle = System.UInt64;
 
     // Wrapper for raylib.dll which is built from my C++ project.
     internal static class RaylibWrapper
     {
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct RendererSettings
+        {
+            internal uint  viewportWidth;
+            internal uint  viewportHeight;
+
+            internal int   samplesPerPixel;
+            internal int   maxPathLength;
+            internal float rayTMin;
+
+            internal uint  renderMode;
+        };
+
         // -----------------------------------------------------------------------
         // Library initialization & termination
 
@@ -29,37 +45,80 @@ namespace gui_app
 
         //Raylib_TransformOBJModel
         //Raylib_FinalizeOBJModel
-        //Raylib_UnloadOBJModel
+
+        [DllImport("raylib.dll")]
+        internal static extern int Raylib_UnloadOBJModel(OBJModelHandle objHandle);
+
         //Raylib_LoadImage
 
         // -----------------------------------------------------------------------
         // Scene
 
-        //Raylib_CreateScene
+        [DllImport("raylib.dll")]
+        internal static extern SceneHandle Raylib_CreateScene();
+
         //Raylib_AddSceneElement
-        //Raylib_AddOBJModelToScene
+
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_AddOBJModelToScene(SceneHandle scene, OBJModelHandle obj);
+
         //Raylib_SetSkyPanorama
         //Raylib_SetSunIlluminance
         //Raylib_SetSunDirection
-        //Raylib_FinalizeScene
-        //Raylib_DestroyScene
-        //Raylib_CreateCamera
-        //Raylib_CameraSetPosition
-        //Raylib_CameraSetLookAt
-        //Raylib_CameraSetPerspective
-        //Raylib_CameraSetLens
-        //Raylib_CameraSetMotion
+
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_FinalizeScene(SceneHandle scene);
+
+        [DllImport("raylib.dll")]
+        internal static extern int Raylib_DestroyScene(SceneHandle scene);
+
+        [DllImport("raylib.dll")]
+        internal static extern CameraHandle Raylib_CreateCamera();
+
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_CameraSetPosition(CameraHandle camera, float x, float y, float z);
+
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_CameraSetLookAt(CameraHandle camera, float x, float y, float z);
+
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_CameraSetPerspective(CameraHandle camera, float fovY_degree, float aspectWH);
+
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_CameraSetLens(CameraHandle camera, float aperture, float focalDistance);
+
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_CameraSetMotion(CameraHandle camera, float beginTime, float endTime);
+
         //Raylib_CameraCopy
-        //Raylib_DestroyCamera
-        //Raylib_CreateImage
-        //Raylib_DestroyImage
+
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_DestroyCamera(CameraHandle camera);
+
+        [DllImport("raylib.dll")]
+        internal static extern ImageHandle Raylib_CreateImage(uint width, uint height);
+
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_DumpImageData(ImageHandle image, float[] imageData);
+
+        [DllImport("raylib.dll")]
+        internal static extern int Raylib_DestroyImage(ImageHandle image);
 
         // -----------------------------------------------------------------------
         // Rendering
 
-        //Raylib_Render
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_Render(
+            ref RendererSettings settings,
+            SceneHandle scene,
+            CameraHandle camera,
+            ImageHandle outMainImage);
+
         //Raylib_Denoise
-        //Raylib_PostProcess
+
+        [DllImport("raylib.dll")]
+        internal static extern void Raylib_PostProcess(ImageHandle image);
+
         //Raylib_IsDenoiserSupported
 
         // -----------------------------------------------------------------------
