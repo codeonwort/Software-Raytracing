86f29026968bb4812f77450aa2ec32e81094598c
codeonwort <codeonwort@gmail.com>
2024-12-05 02:29:54 +0900

    Add controls to containers

--- gui-app/gui-app/MainForm.Designer.cs
+++ gui-app/gui-app/MainForm.Designer.cs
@@ -28,158 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.loggerBox = new System.Windows.Forms.TextBox();
-            this.viewport = new System.Windows.Forms.PictureBox();
-            this.executeButton = new System.Windows.Forms.Button();
-            this.sceneList = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.inputSPP = new System.Windows.Forms.NumericUpDown();
-            this.label3 = new System.Windows.Forms.Label();
-            this.inputPathLen = new System.Windows.Forms.NumericUpDown();
-            ((System.ComponentModel.ISupportInitialize)(this.viewport)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.inputSPP)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.inputPathLen)).BeginInit();
-            this.SuspendLayout();
+            loggerBox = new TextBox();
+            viewport = new PictureBox();
+            executeButton = new Button();
+            sceneList = new ComboBox();
+            sceneList_label = new Label();
+            inputSPP_label = new Label();
+            inputSPP = new NumericUpDown();
+            inputPathLen_label = new Label();
+            inputPathLen = new NumericUpDown();
+            topLevelSplit = new SplitContainer();
+            splitContainer1 = new SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)viewport).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)inputSPP).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)inputPathLen).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)topLevelSplit).BeginInit();
+            topLevelSplit.Panel1.SuspendLayout();
+            topLevelSplit.Panel2.SuspendLayout();
+            topLevelSplit.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
             // 
             // loggerBox
             // 
-            this.loggerBox.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.loggerBox.Location = new System.Drawing.Point(592, 12);
-            this.loggerBox.Multiline = true;
-            this.loggerBox.Name = "loggerBox";
-            this.loggerBox.ReadOnly = true;
-            this.loggerBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.loggerBox.Size = new System.Drawing.Size(398, 486);
-            this.loggerBox.TabIndex = 0;
-            this.loggerBox.WordWrap = false;
+            loggerBox.Dock = DockStyle.Fill;
+            loggerBox.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            loggerBox.Location = new Point(0, 0);
+            loggerBox.Multiline = true;
+            loggerBox.Name = "loggerBox";
+            loggerBox.ReadOnly = true;
+            loggerBox.ScrollBars = ScrollBars.Both;
+            loggerBox.Size = new Size(365, 510);
+            loggerBox.TabIndex = 0;
+            loggerBox.WordWrap = false;
             // 
             // viewport
             // 
-            this.viewport.BackColor = System.Drawing.Color.Black;
-            this.viewport.Location = new System.Drawing.Point(12, 12);
-            this.viewport.Name = "viewport";
-            this.viewport.Size = new System.Drawing.Size(550, 346);
-            this.viewport.TabIndex = 1;
-            this.viewport.TabStop = false;
+            viewport.BackColor = Color.Black;
+            viewport.Dock = DockStyle.Fill;
+            viewport.Location = new Point(8, 8);
+            viewport.Name = "viewport";
+            viewport.Size = new Size(617, 335);
+            viewport.TabIndex = 1;
+            viewport.TabStop = false;
             // 
             // executeButton
             // 
-            this.executeButton.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.executeButton.Location = new System.Drawing.Point(12, 465);
-            this.executeButton.Name = "executeButton";
-            this.executeButton.Size = new System.Drawing.Size(550, 33);
-            this.executeButton.TabIndex = 2;
-            this.executeButton.Text = "Run raytracing";
-            this.executeButton.UseVisualStyleBackColor = true;
-            this.executeButton.Click += new System.EventHandler(this.ExecuteButton_Click);
+            executeButton.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            executeButton.Location = new Point(12, 100);
+            executeButton.Name = "executeButton";
+            executeButton.Size = new Size(550, 33);
+            executeButton.TabIndex = 2;
+            executeButton.Text = "Run raytracing";
+            executeButton.UseVisualStyleBackColor = true;
+            executeButton.Click += ExecuteButton_Click;
             // 
             // sceneList
             // 
-            this.sceneList.FormattingEnabled = true;
-            this.sceneList.Location = new System.Drawing.Point(78, 374);
-            this.sceneList.Name = "sceneList";
-            this.sceneList.Size = new System.Drawing.Size(121, 25);
-            this.sceneList.TabIndex = 3;
+            sceneList.FormattingEnabled = true;
+            sceneList.Location = new Point(78, 10);
+            sceneList.Name = "sceneList";
+            sceneList.Size = new Size(121, 25);
+            sceneList.TabIndex = 3;
             // 
-            // label1
+            // sceneList_label
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(12, 378);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 16);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Scene";
+            sceneList_label.AutoSize = true;
+            sceneList_label.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            sceneList_label.Location = new Point(12, 10);
+            sceneList_label.Name = "sceneList_label";
+            sceneList_label.Size = new Size(47, 16);
+            sceneList_label.TabIndex = 4;
+            sceneList_label.Text = "Scene";
             // 
-            // label2
+            // inputSPP_label
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(247, 378);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(143, 16);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Samples per pixel";
+            inputSPP_label.AutoSize = true;
+            inputSPP_label.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            inputSPP_label.Location = new Point(247, 10);
+            inputSPP_label.Name = "inputSPP_label";
+            inputSPP_label.Size = new Size(143, 16);
+            inputSPP_label.TabIndex = 5;
+            inputSPP_label.Text = "Samples per pixel";
             // 
             // inputSPP
             // 
-            this.inputSPP.Location = new System.Drawing.Point(413, 374);
-            this.inputSPP.Maximum = new decimal(new int[] {
-            4096,
-            0,
-            0,
-            0});
-            this.inputSPP.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.inputSPP.Name = "inputSPP";
-            this.inputSPP.Size = new System.Drawing.Size(120, 25);
-            this.inputSPP.TabIndex = 6;
-            this.inputSPP.Value = new decimal(new int[] {
-            10,
-            0,
-            0,
-            0});
+            inputSPP.Location = new Point(413, 10);
+            inputSPP.Maximum = new decimal(new int[] { 4096, 0, 0, 0 });
+            inputSPP.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            inputSPP.Name = "inputSPP";
+            inputSPP.Size = new Size(120, 25);
+            inputSPP.TabIndex = 6;
+            inputSPP.Value = new decimal(new int[] { 10, 0, 0, 0 });
             // 
-            // label3
+            // inputPathLen_label
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Courier New", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(249, 415);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(127, 16);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Max path length";
+            inputPathLen_label.AutoSize = true;
+            inputPathLen_label.Font = new Font("Courier New", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            inputPathLen_label.Location = new Point(249, 50);
+            inputPathLen_label.Name = "inputPathLen_label";
+            inputPathLen_label.Size = new Size(127, 16);
+            inputPathLen_label.TabIndex = 7;
+            inputPathLen_label.Text = "Max path length";
             // 
             // inputPathLen
             // 
-            this.inputPathLen.Location = new System.Drawing.Point(413, 412);
-            this.inputPathLen.Maximum = new decimal(new int[] {
-            1024,
-            0,
-            0,
-            0});
-            this.inputPathLen.Minimum = new decimal(new int[] {
-            1,
-            0,
-            0,
-            0});
-            this.inputPathLen.Name = "inputPathLen";
-            this.inputPathLen.Size = new System.Drawing.Size(120, 25);
-            this.inputPathLen.TabIndex = 8;
-            this.inputPathLen.Value = new decimal(new int[] {
-            5,
-            0,
-            0,
-            0});
+            inputPathLen.Location = new Point(413, 50);
+            inputPathLen.Maximum = new decimal(new int[] { 1024, 0, 0, 0 });
+            inputPathLen.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            inputPathLen.Name = "inputPathLen";
+            inputPathLen.Size = new Size(120, 25);
+            inputPathLen.TabIndex = 8;
+            inputPathLen.Value = new decimal(new int[] { 5, 0, 0, 0 });
+            // 
+            // topLevelSplit
+            // 
+            topLevelSplit.Dock = DockStyle.Fill;
+            topLevelSplit.IsSplitterFixed = true;
+            topLevelSplit.Location = new Point(0, 0);
+            topLevelSplit.Name = "topLevelSplit";
+            // 
+            // topLevelSplit.Panel1
+            // 
+            topLevelSplit.Panel1.Controls.Add(splitContainer1);
+            // 
+            // topLevelSplit.Panel2
+            // 
+            topLevelSplit.Panel2.Controls.Add(loggerBox);
+            topLevelSplit.Size = new Size(1002, 510);
+            topLevelSplit.SplitterDistance = 633;
+            topLevelSplit.TabIndex = 1;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = DockStyle.Fill;
+            splitContainer1.IsSplitterFixed = true;
+            splitContainer1.Location = new Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            splitContainer1.Orientation = Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(viewport);
+            splitContainer1.Panel1.Padding = new Padding(8);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(sceneList_label);
+            splitContainer1.Panel2.Controls.Add(sceneList);
+            splitContainer1.Panel2.Controls.Add(inputSPP);
+            splitContainer1.Panel2.Controls.Add(inputSPP_label);
+            splitContainer1.Panel2.Controls.Add(inputPathLen_label);
+            splitContainer1.Panel2.Controls.Add(inputPathLen);
+            splitContainer1.Panel2.Controls.Add(executeButton);
+            splitContainer1.Size = new Size(633, 510);
+            splitContainer1.SplitterDistance = 351;
+            splitContainer1.TabIndex = 2;
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1002, 510);
-            this.Controls.Add(this.inputPathLen);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.inputSPP);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.sceneList);
-            this.Controls.Add(this.executeButton);
-            this.Controls.Add(this.viewport);
-            this.Controls.Add(this.loggerBox);
-            this.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Name = "MainForm";
-            this.Text = "MainForm";
-            this.Load += new System.EventHandler(this.MainForm_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.viewport)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.inputSPP)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.inputPathLen)).EndInit();
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1002, 510);
+            Controls.Add(topLevelSplit);
+            Font = new Font("Malgun Gothic", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            Name = "MainForm";
+            Text = "Software Raytracer";
+            Load += MainForm_Load;
+            ((System.ComponentModel.ISupportInitialize)viewport).EndInit();
+            ((System.ComponentModel.ISupportInitialize)inputSPP).EndInit();
+            ((System.ComponentModel.ISupportInitialize)inputPathLen).EndInit();
+            topLevelSplit.Panel1.ResumeLayout(false);
+            topLevelSplit.Panel2.ResumeLayout(false);
+            topLevelSplit.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)topLevelSplit).EndInit();
+            topLevelSplit.ResumeLayout(false);
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
         }
 
         #endregion
@@ -188,10 +220,12 @@
         private PictureBox viewport;
         private Button executeButton;
         private ComboBox sceneList;
-        private Label label1;
-        private Label label2;
+        private Label sceneList_label;
+        private Label inputSPP_label;
         private NumericUpDown inputSPP;
-        private Label label3;
+        private Label inputPathLen_label;
         private NumericUpDown inputPathLen;
+        private SplitContainer topLevelSplit;
+        private SplitContainer splitContainer1;
     }
 }
